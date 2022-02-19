3a1bff642fc489c6c589bf7c34cda1115baa6827
Dain Duncan <dainduncan@gmail.com>
2022-02-19 00:00:41 -0600

    Implemented sessions and leave room functionality

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -11,7 +11,9 @@ export default class HomePage extends Component {
 
     this.state = {
       roomCode: null
-    }
+    };
+    
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -46,6 +48,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    })
+  }
+
   render() {
     return (
         <Router>
@@ -55,7 +63,11 @@ export default class HomePage extends Component {
                 }} />  
                 <Route path='/join' component={RoomJoinPage} />
                 <Route path='/create' component={CreateRoomPage} />
-                <Route path="/room/:roomCode" component={Room} />
+                <Route path="/room/:roomCode" render={(props) => {
+                  return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                  }
+                } 
+                />
             </Switch>
         </Router>
     );
