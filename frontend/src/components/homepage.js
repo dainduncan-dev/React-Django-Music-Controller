1d89c2eea2981ca05687eb7c9db427a255bf7269
Gary Duncan <dainduncan@gmail.com>
2022-02-23 01:45:15 -0600

    Implemented info page

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import RoomJoinPage from "./RoomJoinPage";
+import Info from "./Info";
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
-import Room from "./Room";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -39,6 +40,9 @@ export default class HomePage extends Component {
             <Button color="primary" to="/join" component={Link}>
               Join a Room
             </Button>
+            <Button color="default" to="/info" component={Link}>
+              Info
+            </Button>
             <Button color="secondary" to="/create" component={Link}>
               Create a Room
             </Button>
@@ -62,11 +66,13 @@ export default class HomePage extends Component {
                   return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} />) : ( this.renderHomePage() )
                 }} />  
                 <Route path='/join' component={RoomJoinPage} />
+                <Route path="/info" component={Info} />
                 <Route path='/create' component={CreateRoomPage} />
-                <Route path="/room/:roomCode" render={(props) => {
-                  return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
-                  }
-                } 
+                <Route 
+                  path="/room/:roomCode" 
+                  render={(props) => {
+                    return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                  }} 
                 />
             </Switch>
         </Router>
