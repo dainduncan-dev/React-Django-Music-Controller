56e7091fdbe421ef9341f5b3893765f89fc83453
Gary Duncan <dainduncan@gmail.com>
2022-02-23 01:06:33 -0600

    Implemented Skip button functionality

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -31,6 +31,14 @@ export default class MusicPlayer extends Component {
     fetch("/spotify/play", requestOptions);
   }
 
+  skipSong() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+    };
+    fetch("/spotify/skip", requestOptions)
+  }
+
   render() {
     const songProgress = (this.props.time / this.props.duration) * 100;
 
@@ -55,8 +63,8 @@ export default class MusicPlayer extends Component {
               >
                 {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
               </IconButton>
-              <IconButton>
-                <SkipNextIcon />
+              <IconButton onClick={() => this.skipSong()}>
+                 {this.props.votes} / {" "} {this.props.votes_required} <SkipNextIcon />
               </IconButton>
             </div>
           </Grid>
